a5f209a18b64eb82c9206593bd7c54197fb021a4
Toffee35 <nailzagru@gmail.com>
2025-04-23 12:27:32 +0300

    testing

--- src/bot/messages/commands/user/start.rs
+++ src/bot/messages/commands/user/start.rs
@@ -4,14 +4,31 @@ use teloxide::{
     Bot, RequestError,
     payloads::SendMessageSetters,
     prelude::Requester,
-    types::{InlineKeyboardButton, InlineKeyboardMarkup, Message},
+    types::{KeyboardButton, KeyboardMarkup, Message},
 };
 
 pub async fn start(bot: Bot, msg: Message, db: DbConn) -> Result<(), RequestError> {
     bot.send_message(msg.chat.id, "Выбери модель:")
-        .reply_markup(InlineKeyboardMarkup::new(vec![vec![
-            InlineKeyboardButton::callback("a".to_owned(), "b".to_owned()),
-        ]]))
+        .reply_markup(
+            KeyboardMarkup::new(vec![
+                vec![
+                    KeyboardButton::new("1a"),
+                    KeyboardButton::new("2a"),
+                    KeyboardButton::new("3a"),
+                ],
+                vec![
+                    KeyboardButton::new("1b"),
+                    KeyboardButton::new("2b"),
+                    KeyboardButton::new("3b"),
+                ],
+                vec![
+                    KeyboardButton::new("1c"),
+                    KeyboardButton::new("2c"),
+                    KeyboardButton::new("3c"),
+                ],
+            ])
+            .resize_keyboard(),
+        )
         .await?;
 
     Ok(())
